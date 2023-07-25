403d7fb87ddd2c00452ef6d76bc6e3333a03015d
BranBayou <berhan.baye@gmail.com>
2023-07-25 16:10:14 +0300

    Implemented requested changes

--- db/migrate/20230724205840_create_posts.rb
+++ db/migrate/20230724205840_create_posts.rb
@@ -3,7 +3,9 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.references :user, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }
+      t.integer :comments_counter
+      t.integer :likes_counter
 
       t.timestamps
     end
